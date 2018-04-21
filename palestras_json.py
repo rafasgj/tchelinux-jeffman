8fc27ce5d8f9f7a4dcdf508a8808d5e3d09fd870
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-04-21 16:41:41 -0300

    Corrigida exibicao do "diretorio academico". Corrigido codigo de acordo com PEP8 e o Flake8.

--- palestras_json.py
+++ palestras_json.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python3
 
+"""Load a CSV file with lectures and provide a JSON file."""
+
 import json
 import csv
 import sys
 import re
-from functools import reduce
+
 
 def load_data(eventfile):
+    """Load data from file."""
     lectures = {}
-    fields = ["room", "author", "title", "abstract", "keywords", "level", "resume"]
-    with open('data/%s.csv'%eventfile) as csvfile:
+    fields = ["room", "author", "title", "abstract", "keywords",
+              "level", "resume"]
+    with open('data/%s.csv' % eventfile) as csvfile:
         for row in csv.reader(csvfile):
-            if re.search(r'timestamp',"".join(row),re.I): continue
-            passed_header = True
-            if row[0] == '': continue
-            p = { k:v for k,v in zip(fields,row[1:8]) }
-            lectures.setdefault(row[0],[]).append(p)
+            if row[0] == '':
+                continue
+            if re.search(r'timestamp', "".join(row), re.I):
+                continue
+            if row[0][0] != '1':
+                row[0] = '0'+row[0]
+            p = {k: v for k, v in zip(fields, row[1:8])}
+            lectures.setdefault(row[0], []).append(p)
     return lectures
 
+
 def load_event(eventfile):
-    with open('data/%s.json'%eventfile) as jf:
+    """Load an event."""
+    with open('data/%s.json' % eventfile) as jf:
         return json.load(jf)
 
+
 if len(sys.argv) < 2:
-    print ("usage: palestras_json.py <codinome>")
-    print ("\nNao esqueca de adicionar os aquivos JSON e CSV ao diretario 'data'")
+    print("usage: palestras_json.py <codinome>\n")
+    msg = "Nao esqueca de adicionar os aquivos JSON e CSV ao diretario 'data'"
+    print(msg)
     sys.exit()
 
 lectures = load_data(sys.argv[1])
