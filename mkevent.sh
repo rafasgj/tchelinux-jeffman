a08087a9077851a4761b6a42d29e0d5cb1344ab4
Rafael Guterres Jeffman <rafasgj@gmail.com>
2018-06-12 15:05:27 -0300

    Allow creation of multiple hotsites simultaneously.

--- mkevent.sh
+++ mkevent.sh
@@ -1,28 +1,30 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
-    echo -e "\nusage:\n\t mkevent <codiname>\n"
+    echo -e "\nusage:\n\t mkevent <codiname> ...\n"
     exit 1
 fi
 
 base="`dirname $0`"
 
-codiname=$1
-shift
+while [ ! -z "$1" ]
+do
+    codiname=$1
+    shift
 
-echo "Creating event: ${codiname}"
+    echo "Creating event: ${codiname}"
 
-if ! "$base/tchelinux-event.py" ${codiname}
-then
-    echo "Failed to generate hotsite."
-    exit 1
-fi
+    if ! "$base/tchelinux-event.py" ${codiname}
+    then
+        echo "Failed to generate hotsite."
+        exit 1
+    fi
 
-[ -d ${codiname} ] && rm -rf ${codiname}
-mkdir -p ${codiname}/data
-mv CNAME index.html ${codiname}
-cp -r template/* ${codiname}
-[ -d "images" ] && cp -r images ${codiname} || mkdir ${codinome}/images
-cp data/${codiname}.json ${codiname}/data/${codiname}.json
-
+    [ -d ${codiname} ] && rm -rf ${codiname}
+    mkdir -p ${codiname}/data
+    mv CNAME index.html ${codiname}
+    cp -r template/* ${codiname}
+    [ -d "images" ] && cp -r images ${codiname} || mkdir ${codinome}/images
+    cp data/${codiname}.json ${codiname}/data/${codiname}.json
+done
